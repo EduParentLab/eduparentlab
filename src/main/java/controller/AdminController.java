dfab781053483cad6943cd0a47cc7f5032beb7bf
rmadudbb-test <rmadudbb@gmail.com>
2025-08-26 16:21:29 +0900

    관리자 페이지 비동기 탭까지 완성함

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -7,13 +7,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.service.PostService;
+import model.service.UserService;
 import domain.Post;
+import domain.User;
 import java.io.IOException;
 import java.util.ArrayList;
 
 
 
-@WebServlet("admin/admin.do")
+@WebServlet("/admin/admin.do")
 public class AdminController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -22,24 +24,32 @@ public class AdminController extends HttpServlet {
     }
 
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String m = request.getParameter("m");
-		if(m != null) {
-			m = m.trim();
-			switch(m){
-				case "getNews": getNews(request, response); break;
-				default: getNews(request, response);
-			}
-		}
+			
+	    getNews(request, response);
+		getUser(request, response);
+		getGhost(request, response);
+		String view = "admin.jsp";
+	    RequestDispatcher rd = request.getRequestDispatcher(view);
+	    rd.forward(request, response);
 	}
     private void getNews(HttpServletRequest request, HttpServletResponse response) 
 	        throws ServletException, IOException {
 	    PostService service = PostService.getInstance();
-	    ArrayList<Post> list = service.listS();
-	    request.setAttribute("list", list);
+	    ArrayList<Post> postlist = service.listS();	   
+	    request.setAttribute("news", postlist);	    
 	    
-	    String view = "list.jsp";
-	    RequestDispatcher rd = request.getRequestDispatcher(view);
-	    rd.forward(request, response);
 	}
-
+    private void getUser(HttpServletRequest request, HttpServletResponse response) 
+	        throws ServletException, IOException {
+	    UserService service = UserService.getInstance();
+	    ArrayList<User> userlist = service.listS();	   
+	    request.setAttribute("user", userlist);	    
+	    
+	}
+    private void getGhost(HttpServletRequest request, HttpServletResponse response) 
+	        throws ServletException, IOException {
+	    UserService service = UserService.getInstance();
+	    ArrayList<User> ghostlist = service.listGhostUserS();	   
+	    request.setAttribute("ghost", ghostlist);	   
+	}
 }
---
+++ src/main/java/model/dao/UserDAO.java
@@ -0,0 +1,87 @@
+package model.dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import javax.naming.*;
+import javax.sql.DataSource;
+
+import domain.User;
+import static model.sql.PostSQL.*;
+
+public class UserDAO {
+	private DataSource ds;
+	public UserDAO() {
+		try{
+			Context initContext = new InitialContext();
+			Context envContext = (Context)initContext.lookup("java:/comp/env");
+			ds = (DataSource)envContext.lookup("jdbc/TestDB");
+		}catch(NamingException ne){
+		}
+	}
+	public ArrayList<User> list(){
+		ArrayList<User> list = new ArrayList<User>();
+		Connection con = null;
+		Statement stmt = null;
+	    ResultSet rs = null;
+	    try{
+			con = ds.getConnection();
+			stmt = con.createStatement();
+	        rs = stmt.executeQuery(USERLIST);
+	        while(rs.next()){
+	        	String email = rs.getString(1);
+	        	String password = rs.getString(2);
+	        	String nickname = rs.getString(3);
+	        	String gender = rs.getString(4);
+	        	java.sql.Date birth = rs.getDate(5);
+	        	String name = rs.getString(6);
+	        	String phone = rs.getString(7);
+	        	java.sql.Date cdate = rs.getDate(8);
+	        	int role_num = rs.getInt(9);
+	            
+	            list.add(new User(email, password, nickname, gender, birth, name, phone, cdate, role_num));
+	        }
+	        return list;
+        }catch(SQLException se){
+            return null;
+        }finally{
+            try{
+                rs.close();
+                stmt.close();
+                con.close();
+            }catch(SQLException se){}
+        }		
+	}
+	public ArrayList<User> listGhostUser(){
+		ArrayList<User> list = new ArrayList<User>();
+		Connection con = null;
+		Statement stmt = null;
+	    ResultSet rs = null;
+	    try{
+			con = ds.getConnection();
+			stmt = con.createStatement();
+	        rs = stmt.executeQuery(GHOSTUSERLIST);
+	        while(rs.next()){
+	        	String email = rs.getString(1);
+	        	String password = rs.getString(2);
+	        	String nickname = rs.getString(3);
+	        	String gender = rs.getString(4);
+	        	java.sql.Date birth = rs.getDate(5);
+	        	String name = rs.getString(6);
+	        	String phone = rs.getString(7);
+	        	java.sql.Date cdate = rs.getDate(8);
+	        	int role_num = rs.getInt(9);
+	            
+	            list.add(new User(email, password, nickname, gender, birth, name, phone, cdate, role_num));
+	        }
+	        return list;
+        }catch(SQLException se){
+            return null;
+        }finally{
+            try{
+                rs.close();
+                stmt.close();
+                con.close();
+            }catch(SQLException se){}
+        }		
+	}	
+}
