8d76d0ade6b52fe1855fbea046a137f2ffa3ec9e
dusgml4258-test <dusgml4258@gmail.com>
2025-09-04 16:58:39 +0900

    답댓글 수정 삭제

--- src/main/webapp/post/js/board_content.js
+++ src/main/webapp/post/js/board_content.js
@@ -56,8 +56,13 @@ function events(){
 			e.preventDefault();
 	        const $form = $(this);
 	        const postNum = $form.find('input[name="post_num"]').val();
-	        const content = $form.find('textarea[name="content"]').val();
-
+	        const content = $form.find('textarea[name="content"]').val().trim();
+			// 빈 댓글 처리
+		   if(content === "") {
+		       alert("내용을 입력해주세요.");
+		       $form.find('textarea[name="content"]').focus();
+		       return; // submit 중단
+		   }
 			$.post(`${BASE_URL}/comment/comment.do?m=insert`, 
 			    { post_num: postNum, content: content }
 			).done(function() {
@@ -91,8 +96,13 @@ function events(){
 		    const $form = $(this);
 		    const postNum = $form.find('input[name="post_num"]').val();
 		    const parentNum = $form.find('input[name="parent_num"]').val();
-		    const content = $form.find('textarea[name="content"]').val();
-
+		    const content = $form.find('textarea[name="content"]').val().trim();
+			// 빈 댓글 처리
+		   if(content === "") {
+		       alert("댓글 내용을 입력해주세요.");
+		       $form.find('textarea[name="content"]').focus();
+		       return; // submit 중단
+		   }
 			$.post(`${BASE_URL}/comment/comment.do?m=recomment`, 
 			    { post_num: postNum, parent_num: parentNum, content: content }
 			).done(function() {
@@ -123,18 +133,21 @@ function events(){
 		// 댓글 수정 버튼 클릭
 		$(document).on("click", ".editBtn", function() {
 		    const $btn = $(this);
-		    const $commentDiv = $btn.closest(".section-content-comment"); // li 대신 div 선택
+		    const $commentDiv = $btn.closest(".section-content-comment, .section-content-recomment"); // li 대신 div 선택
 		    const commentNum = $commentDiv.data("comment-num");
 		    
 		    $.get(`${BASE_URL}/comment/comment.do`, { m: "checkUpdateAuth", comment_num: commentNum })
 		    	.done(function() {
-			        const $content = $commentDiv.find("> .comment-original > div:nth-child(2) > .content"); // content span 선택
-			        const oldText = $content.text();
+			        let $content = $commentDiv.find("> .comment-original > div:nth-child(2) > .content"); // content span 선택
+					if($content.length === 0){
+					    $content = $commentDiv.find("> .recomment-original .content");
+					}
+					const oldText = $content.text();
 
 			        // span → input
 					const $textarea = $('<textarea></textarea>')
 					    .val(oldText)
-					    .attr('rows', 4)           // 높이: 4줄
+					    .attr('rows', 3)           // 높이: 4줄
 					    .attr('cols', 50)          // 너비: 50글자 기준
 					    .css({
 					        width: '100%',         // div에 맞춰 꽉 채우기
@@ -152,10 +165,10 @@ function events(){
 		    	})
 			});
 	
-			// 댓글 저장 버튼 클릭
+			//댓글 수정 후 등록 버튼 클릭
 			$(document).on("click", ".saveBtn", function() {
 			    const $btn = $(this);
-			    const $commentDiv = $btn.closest(".section-content-comment"); // li 대신 div 선택
+			    const $commentDiv = $btn.closest(".section-content-comment, .section-content-recomment"); // li 대신 div 선택
 			    const $textarea = $commentDiv.find("textarea");
 			    const newText = $textarea.val();
 			    const commentNum = $commentDiv.data("comment-num");
@@ -165,7 +178,11 @@ function events(){
 			    $.post(`${BASE_URL}/comment/comment.do?m=update`, 
 			        { comment_num: commentNum, comment_content: newText },
 			        function() {
-			            $commentDiv.find("> .comment-original > div:nth-child(2) > .content").text(newText);
+			            let $content = $commentDiv.find("> .comment-original > div:nth-child(2) > .content");
+						if($content.length === 0){
+						    $content = $commentDiv.find("> .recomment-original .content");
+						}
+						$content.text(newText);
 			            $btn.text("수정").removeClass("saveBtn").addClass("editBtn");
 			        }
 			    ).fail(function() {
@@ -176,7 +193,7 @@ function events(){
 		    // 댓글 삭제 버튼 클릭
 		    $(document).on("click", ".deleteBtn", function() {
 				const $btn = $(this);
-				    const $commentDiv = $btn.closest(".section-content-comment"); // ✅ li 대신 div 선택
+				    const $commentDiv = $btn.closest(".section-content-comment, .section-content-recomment"); // ✅ li 대신 div 선택
 				    const commentNum = $commentDiv.data("comment-num");           // data-comment-num 읽기
 
 				    console.log("삭제 시도 commentNum:", commentNum);
